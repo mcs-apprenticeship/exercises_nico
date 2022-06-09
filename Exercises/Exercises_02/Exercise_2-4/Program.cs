afeabbbf270d814f0fdff4b834eda86b629ba5ed
Nico Egli <nico.egli@mcs.ch>
2022-06-09 13:58:46 +0200

    Exercise 4 done

---
+++ Exercises/Exercises_02/Exercise_2-4/Program.cs
@@ -0,0 +1,37 @@
+﻿class Sum
+{
+    public static void Main()
+    {
+        Sum sum = new Sum();
+        sum.start();
+    }
+
+    private void start()
+    {
+        Console.WriteLine("sum()");
+        Console.WriteLine(sum());
+        Console.WriteLine("sum(7)");
+        Console.WriteLine(sum(7));
+        Console.WriteLine("sum(9, 11)");
+        Console.WriteLine(sum(9, 11));
+        Console.WriteLine("sum(1, 2, 3, 4, 5)");
+        Console.WriteLine(sum(1, 2, 3, 4, 5));
+        Console.WriteLine("sum(\"ab\", \"cdef\")");
+        Console.WriteLine(sum("ab", "cdef"));
+    }
+
+    private int sum (params int[] values)
+    {
+        return values.Sum();
+    }
+
+    private int sum(int value0, int value1)
+    {
+        return -1;
+    }
+
+    private int sum(string value0, string value1)
+    {
+        return value0.Length + value1.Length;
+    }
+}
