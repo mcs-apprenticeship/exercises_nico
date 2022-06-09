d6ab4b90464f17907a80df60b19c415a2509378f
Nico Egli <nico.egli@mcs.ch>
2022-06-09 14:56:19 +0200

    Exercise 6 done

---
+++ Exercises/Exercises_02/Exercise_2-6/Program.cs
@@ -0,0 +1,46 @@
+﻿class PerfectNumbers
+{
+    public static void Main ()
+    {
+        PerfectNumbers perfectNumbers = new PerfectNumbers();
+        perfectNumbers.start ();
+    }
+
+    private void start()
+    {
+        int[] perfectNumbers = getPerfectNumbers(4);
+
+        foreach(int num in perfectNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    private int[] getPerfectNumbers(int amount)
+    {
+        List<int> perfectNumbers = new List<int>();
+        int i = 2;
+        while (perfectNumbers.Count() < amount)
+        {
+            if(i == getAllDivisors(i).Sum())
+            {
+                perfectNumbers.Add(i);
+            }
+            i++;
+        }
+        return perfectNumbers.ToArray();
+    }
+
+    private int[] getAllDivisors (int num)
+    {
+        List<int> divisors = new List<int> ();
+        for(int i = 1; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                divisors.Add(i);
+            }
+        }
+        return divisors.ToArray ();
+    }
+}
