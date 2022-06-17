d226794480c8b9078322332d970a9eefe334f3b9
neg_mcs <nico.egli@mcs.ch>
2022-06-17 14:41:31 +0200

    exercise 2 done

---
+++ Exercises/Exercises_03/Exercise_3-2/Program.cs
@@ -0,0 +1,302 @@
+﻿namespace Exercises_03{
+    class Program
+    {
+        private static Deadline dl = new(
+            new DateTime(),
+            new Duration()
+        );
+
+        private static bool dlSet = false;
+
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (true)
+            {
+                if (!dlSet)
+                {
+                    SetDate();
+                    SetTime();
+                    dlSet = true;
+                }
+
+                PrintHead();
+                int input = UserInput.choose(PrintHead, "reset date", "reset time", "reload");
+                int i = input;
+                switch (input)
+                {
+                    case 1:
+                        SetDate();
+                        break;
+                    case 2:
+                        SetTime();
+                        break;
+                }
+            }
+        }
+
+        private static string GetWithZeros(int num, int digits)
+        {
+            int amount = digits - num.ToString().Length;
+            return String.Concat(Enumerable.Repeat("0", amount)) + num;
+        }
+
+        private static string GetHours()
+        {
+            return GetWithZeros((int)Math.Floor(dl.duration.GetDuration(Duration.Units.h)), 2);
+        }
+
+        private static string GetMins()
+        {
+            return GetWithZeros((int)(Math.Floor(dl.duration.GetDuration(Duration.Units.m) - Math.Floor(dl.duration.GetDuration(Duration.Units.h)) * 60)), 2);
+        }
+
+        private static string GetSecs()
+        {
+            return GetWithZeros((int)(Math.Floor(dl.duration.GetDuration(Duration.Units.s) - Math.Floor(dl.duration.GetDuration(Duration.Units.m)) * 60)), 2);
+        }
+
+        private static string GetYears()
+        {
+            return GetWithZeros(dl.start.Year, 4);
+        }
+
+        private static string GetMonths()
+        {
+            return GetWithZeros(dl.start.Month, 2);
+        }
+
+        private static string GetDays()
+        {
+            return GetWithZeros(dl.start.Day, 2);
+        }
+
+        private static void PrintInDeadline()
+        {
+            if (dl.InDeadline())
+            {
+                UiUtils.PrintLine("In deadline!", ConsoleColor.Green);
+            }
+            else
+            {
+                UiUtils.PrintLine("Not in deadline!", ConsoleColor.Red);
+            }
+        }
+
+        private static object? PrintHead()
+        {
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}/{GetMonths()}/{GetDays()}");
+            UiUtils.PrintLine($"Time: {GetHours()}:{GetMins()}:{GetSecs()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            PrintInDeadline();
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            return null;
+        }
+
+        private static void SetTime()
+        {
+            dl.ResetTime();
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}/{GetMonths()}/{GetDays()}");
+            UiUtils.PrintLine($"Time:");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Hours?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int h = UserInput.GetInt(IsValidHour);
+            dl.SetHour(h);
+            Console.Clear();
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}/{GetMonths()}/{GetDays()}");
+            UiUtils.PrintLine($"Time: {GetHours()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Minutes?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int m = UserInput.GetInt(IsValidMin);
+            dl.SetMin(m);
+            Console.Clear();
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}/{GetMonths()}/{GetDays()}");
+            UiUtils.PrintLine($"Time: {GetHours()}:{GetMins()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Seconds?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int s = UserInput.GetInt(IsValidSec);
+            dl.SetSec(s);
+        }
+
+        private static void SetDate()
+        {
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine("Date:");
+            if (dlSet) UiUtils.PrintLine($"Time: {GetHours()}:{GetMins()}:{GetSecs()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Date year?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int year = UserInput.GetInt(IsValidYear);
+            dl.SetYear(year);
+            Console.Clear();
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}");
+            if (dlSet) UiUtils.PrintLine($"Time: {GetHours()}:{GetMins()}:{GetSecs()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Date Month?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int month = UserInput.GetInt(IsValidMonth);
+            dl.SetMonth(month);
+            Console.Clear();
+            UiUtils.PrintHeader();
+            UiUtils.PrintLine($"Date: {GetYears()}/{GetMonths()}");
+            if (dlSet) UiUtils.PrintLine($"Time: {GetHours()}:{GetMins()}:{GetSecs()}");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            UiUtils.PrintLine("Date Day?");
+            UiUtils.PrintBorder(UiUtils.BorderType.Light);
+            int day = UserInput.GetInt(IsValidDay);
+            dl.SetDay(day);
+        }
+
+        private static bool IsValidYear(int year)
+        {
+            return year > 0 && year < 10000;
+        }
+
+        private static bool IsValidMonth(int month)
+        {
+            if(month > 0 && month <= 12)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidDay(int day)
+        {
+            if (day > 0 && day <= 31)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidHour(int h)
+        {
+            if (h >= 0 && h < 24)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidMin(int m)
+        {
+            if (m >= 0 && m < 60)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidSec(int s)
+        {
+            if (s >= 0 && s < 60)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    
+
+    class Deadline
+    {
+        public DateTime start { get; private set; }
+        public Duration duration { get; private set; }
+
+        public Deadline(DateTime start, Duration duration)
+        {
+            this.start = start;
+            this.duration = duration;
+        }
+
+        public void ResetTime()
+        {
+            duration = new Duration();
+        }
+
+        public void SetYear(int year)
+        {
+            start = new DateTime(year, start.Month, start.Day);
+        }
+
+        public void SetMonth(int month)
+        {
+            start = new DateTime(start.Year, month, start.Day);
+        }
+
+        public void SetDay(int day)
+        {
+            start = new DateTime(start.Year, start.Month, day);
+        }
+
+        public void SetHour(int h)
+        {
+            duration.AddSeconds(h * 3600);
+        }
+
+        public void SetMin(int m)
+        {
+            duration.AddSeconds(m * 60);
+        }
+
+        public void SetSec(int s)
+        {
+            duration.AddSeconds(s);
+        }
+
+        public Boolean InDeadline()
+        {
+            return start.AddSeconds(duration.GetDuration(Duration.Units.s)) > DateTime.Now;
+        }
+    }
+
+    class Duration
+    {
+        public enum Units
+        {
+            s,
+            m,
+            h
+        }
+
+        private int durationS;
+
+        public double GetDuration(Units chosenUnit)
+        {
+            return chosenUnit switch
+            {
+                Units.s => durationS,
+                Units.m => (double)durationS / 60,
+                Units.h => (double)durationS / 3600,
+                _ => 0,
+            };
+        }
+        public void SetDuration(double amount,Units chosenUnit)
+        {
+            durationS = chosenUnit switch
+            {
+                Units.s => (int)amount,
+                Units.m => (int)(amount * 60),
+                Units.h => (int)(amount * 3600),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public void AddSeconds (int amount)
+        {
+            durationS += amount;
+        }
+    }
+}
---
+++ Exercises/Utils/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Exercises_03
+{
+    public class Program { public static void Main () { } }
+
+    public static class UserInput
+    {
+        public static int GetInt(Func<int, bool> validator)
+        {
+            string numAsString = string.Empty;
+
+            UiUtils.StartLine(": ", UiUtils.BorderType.Bottom);
+            while (true)
+            {
+                ConsoleKeyInfo input = Console.ReadKey();
+                if (int.TryParse(input.KeyChar.ToString(), out _))
+                {
+                    numAsString += input.KeyChar;
+                }
+                else if (input.Key == ConsoleKey.Enter)
+                {
+                    if (numAsString.Length > 0)
+                    {
+                        int num = int.Parse(numAsString);
+                        if (validator(num))
+                        {
+                            return num;
+                        }
+                    }
+                    numAsString = String.Empty;
+                    UiUtils.StartLine(": ", UiUtils.BorderType.Bottom);
+                }
+                else
+                {
+                    if (input.Key == ConsoleKey.Backspace)
+                    {
+                        numAsString = numAsString.Remove(numAsString.Length - 1);
+                    }
+                    UiUtils.ClearCurrentConsoleLine();
+                    UiUtils.StartLine($": {numAsString}", UiUtils.BorderType.Bottom);
+                }
+            }
+        }
+
+        public static int choose(Func<object?> printBefore, params string[] choices)
+        {
+            int input = 0;
+            while (input > choices.Length || input < 1)
+            {
+                Console.Clear();
+                printBefore();
+
+                for (int i = 1; i <= choices.Length; i++)
+                {
+                    UiUtils.PrintLine($" {i}: {choices[i - 1]}");
+                }
+
+
+                UiUtils.PrintBorder(UiUtils.BorderType.Light);
+                UiUtils.StartLine(": ", UiUtils.BorderType.Bottom);
+                string inputString = Console.ReadLine();
+                int.TryParse(inputString, out input);
+            }
+            return input;
+        }
+    }
+
+    public static class UiUtils
+    {
+        public enum BorderType
+        {
+            Light,
+            Bold,
+            Top,
+            Bottom,
+        }
+
+        private const int width = 40;
+
+        public static void PrintHeader()
+        {
+            Console.Clear();
+            PrintBorder(BorderType.Top);
+            PrintLine("Deadline");
+            PrintBorder(BorderType.Bold);
+        }
+
+        public static void PrintLine(string content, ConsoleColor color = ConsoleColor.White)
+        {
+            ConsoleColor colorBefore = Console.ForegroundColor;
+            Console.Write("┃");
+            Console.ForegroundColor = color;
+            Console.Write($"{content}{String.Concat(Enumerable.Repeat(" ", (width - content.Length)))}");
+            Console.ForegroundColor = colorBefore;
+            Console.WriteLine("┃");
+        }
+
+        public static void StartLine(string content, BorderType borderAfter)
+        {
+            PrintLine(content);
+            PrintBorder(borderAfter);
+            Console.SetCursorPosition(content.Length + 1, Console.CursorTop - 2);
+
+        }
+
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        public static void PrintBorder(BorderType bt)
+        {
+            switch (bt)
+            {
+                case BorderType.Light:
+                    PrintBorder('┠', '─', '┨');
+                    break;
+                case BorderType.Bold:
+                    PrintBorder('┣', '━', '┫');
+                    break;
+                case BorderType.Top:
+                    PrintBorder('┏', '━', '┓');
+                    break;
+                case BorderType.Bottom:
+                    PrintBorder('┗', '━', '┛');
+                    break;
+            }
+        }
+        private static void PrintBorder(char first, char border, char last)
+        {
+            Console.WriteLine($"{first}{String.Concat(Enumerable.Repeat(border, width))}{last}");
+        }
+    }
+}
