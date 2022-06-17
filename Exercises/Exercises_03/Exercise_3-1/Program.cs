e24e8f09a60cc96f82cb3f4bf8edbc409c8b86d4
neg_mcs <nico.egli@mcs.ch>
2022-06-17 08:39:43 +0200

    triangle is drawn

--- Exercises/Exercises_03/Exercise_3-1/Program.cs
+++ Exercises/Exercises_03/Exercise_3-1/Program.cs
@@ -2,33 +2,38 @@
 {
     public static void Main ()
     {
-        point p1 = new()
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Point p1 = new()
         {
             x = 1,
             y = 1
         };
-        point p2 = new()
+        Point p2 = new()
         {
             x = 7,
             y = 3
         };
-        point p3 = new()
+        Point p3 = new()
         {
             x = 7,
             y = 3
         };
 
-        Console.WriteLine (Geometry.perimeterTriangle (p1, p2, p3));
+        Console.WriteLine ($"A = {Geometry.perimeterTriangle (p1, p2, p3)}");
+        Geometry.printTriangle(p1, p2, p3);
 
+        Console.WriteLine ();
         p3.x = 4;
 
         Console.WriteLine(Geometry.perimeterTriangle(p1, p2, p3));
+        Geometry.printTriangle(p1, p2, p3);
     }
 }
 
 public static class Geometry
 {
-    private static double distance (point point1, point point2)
+    private static double distance (Point point1, Point point2)
     {
         double xDistance = point1.x - point2.x;
         double yDistance = point1.y - point2.y;
@@ -37,13 +42,55 @@ public static class Geometry
         return result;
     }
 
-    public static double perimeterTriangle(point point1, point point2, point point3)
+    public static double perimeterTriangle(Point point1, Point point2, Point point3)
     {
         return distance(point1, point2) + distance(point1, point3) + distance(point2, point3);
     }
+
+    public static void printTriangle(Point point1, Point point2, Point point3)
+    {
+        List<Point> points = new(){ point1, point2, point3 };
+
+        Point? pointLeft = null;
+        double minDistance = 0;
+        foreach(Point point in points )
+        {
+            double distanceToZero = distance(point, new Point() { x = 0, y = 0 });
+            if(distanceToZero < minDistance || pointLeft == null)
+            {
+                pointLeft = point;
+                minDistance = distanceToZero;
+            }
+        }
+
+        points.Remove(pointLeft);
+
+        Point? pointRight = null;
+        minDistance = 0;
+        foreach(Point point in points)
+        {
+            double distanceToY = point.y;
+            if (distanceToY < minDistance || pointRight == null)
+            {
+                pointRight = point;
+                minDistance = distanceToY;
+            }
+        }
+
+        points.Remove(pointRight);
+
+        Point pointTop = points[0];
+
+        Console.WriteLine($"    {pointTop.x}/{pointTop.y}");
+        Console.WriteLine($"    ╱╲");
+        Console.WriteLine($"   ╱  ╲");
+        Console.WriteLine($"  ╱    ╲");
+        Console.WriteLine($" ╱______╲");
+        Console.WriteLine($"{pointLeft.x}/{pointLeft.y}   {pointRight.x}/{pointRight.y}");
+    }
 }
 
-public class point
+public class Point
 {
     public int x;
     public int y;
